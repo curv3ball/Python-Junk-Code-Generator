a8345aea500222bf54c5dd1f7358fb2b8434732d
Trey <39141616+curv3ball@users.noreply.github.com>
2023-08-07 01:32:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 from junk_generator import junk_generator as generator
  
-generator.generate(
-    amount=5,
-    gDocstring=True,
-    gTypehint=True,
-    gTypecheck=True,
-    gPrint=False,
-    gReturn=True,
-    gFillLists=True,
-    gFillDicts=True,
-    gMaxListEntries=5,
-    gMaxDictEntries=5,
-    save_file="output.txt"
-)
+generator.generate(amount=5)
